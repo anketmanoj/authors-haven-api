9189f56f5e0973173627792affabff71a8a83a7f
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2023-03-07 00:14:13 +0900

    chore: setup basic django project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+from .base import env
+
+DEBUG = True
+
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="!@#$%^&*()_+jkdnkjdnijdksndskjbnsdcoiuwehiuewneduknke,kscdniukj")
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1"
+]
