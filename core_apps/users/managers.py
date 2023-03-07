92945e7d0550b79b96eedfb5a65a4ec02c2796e4
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2023-03-07 22:21:14 +0900

    feat: add custom user model, setup redoc api doc

---
+++ core_apps/users/managers.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import ugettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, first_name, last_name, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('The Email must be set'))
+        if not username:
+            raise ValueError(_('The Username must be set'))
+        if not first_name:
+            raise ValueError(_('The First Name must be set'))
+        if not last_name:
+            raise ValueError(_('The Last Name must be set'))
+
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        if not password:
+            raise ValueError(_('The Password must be set'))
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Admin account must have an email address.'))
+
+        user = self.create_user(username, first_name, last_name, email, password, **extra_fields)
+        user.save(using=self._db)
+
+        return user
+
+        
+
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_('The Email is not valid'))
--- core_apps/users/models.py
+++ core_apps/users/models.py
@@ -1,3 +1,36 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True,editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(verbose_name= _('username'), db_index=True, max_length=255)
+    first_name = models.CharField(verbose_name=_('first name'), max_length=50)
+    last_name = models.CharField(verbose_name=_('last name'), max_length=50)
+    email = models.EmailField(verbose_name=_('email address'), db_index=True, unique=True)
+    is_staff = models.BooleanField(verbose_name=_('staff status'), default=False, help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(verbose_name=_('active'), default=True, help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(verbose_name=_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+    def get_short_name(self):
+        return self.first_name
